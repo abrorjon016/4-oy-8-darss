808efdfa02432bf58477c3c69ff3d9a6ae84251d
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-10-08 22:46:22 +0500

    qoshimcha darslar ishlandadi

--- js/index.js
+++ js/index.js
@@ -1,2 +1,131 @@
+// 1-masala
+// console.log(Math.floor(Math.random() * 100) + 1);
+
+// 2-masala
+// ???
+
+// 3-masala
+// ???
+
+// 4-masala
+// let num = +prompt("son")
+// console.log(Math.floor(num));
+// console.log(Math.round(num));
+
+// 5-masala
+// umuman tushunmadim
+
+// Ternary
 // 1 - masala
-// start at 11:00  I am so sorry.
+// let num = prompt("sonni kiriting");
+// console.log(num >= 0 ? "musbat" : "manfiy");
+
+// 2 - masala
+// let age = prompt("sonni kiriting");
+// console.log(age >= 18 ? "ruxsat" : "ruxsat yoq");
+
+// 3 - masala
+// let num = prompt("sonni kiritingh");
+// console.log(num % 2 == 0 ? "juft" : "toq");
+
+// for sikl
+
+// 1 - masala
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0 && i > 0) {
+//     console.log(i);
+//   }
+// }
+
+// 2 - masala
+// let num = 0;
+// for (let i = 1; i <= 10; i++) {
+//   let num2 = prompt(i + "sonni kiriting");
+//   if (num2 > 0) num += num2;
+// }
+// console.log(num);
+
+// 3 - masala
+// for (let i = 1; i <= 100; i++) {
+//   if (i >= 30 && i <= 50) {
+//     console.log(i);
+//   }
+// }
+
+// 4 - masala
+// qilalmadim
+
+// 5 yoq
+
+// 6 - masala
+// let num = 0;
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 !== 0) {
+//     num += i;
+//   }
+// }
+// console.log(num);
+
+// 7-masala
+// for (let i = 1; i <= 5; i++) {
+//   let yosh = prompt(i + "sonni kriting");
+
+//   if (yosh >= 18) {
+//     console.log("voyaga yetgan");
+//   } else {
+//     console.log("voyaga yetmagan");
+//   }
+// }
+
+// 8-masala
+// let counter = 0;
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 == 0) {
+//     counter++;
+//   }
+// }
+// console.log(counter);
+
+// 9 - masala
+// let counter = 0;
+// for (let i = 1; i <= 10; i++) {
+//   let num = +prompt("${i} sonni kriting");
+//   if (i > 5 && i < 10) {
+//     counter++;
+//   }
+// }
+// console.log(counter);
+
+// 10-masala
+// for(let i=1; i <= 10; i++){
+//     let num = +prompt("${i} sonni kriting")
+//     if (i > 0) {
+//         console.log(num);
+//     }
+// }
+
+// 11-masala
+// for(let i=1; i <= 15; i++){
+//     let num = +prompt("${i} sonni kiriting")
+//     if (i > 0 || i < 0) {
+//         console.log(num);
+//     }
+// }
+
+// 12 - masala
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// qoshimcha masalalar
+
+// 1-masala
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 == 0) {
+//     console.log("num ${i}, kvadrati: ${Math.pow(i, 2)}");
+//   } else {
+//     console.log("num ${i}, kvadrat ildizi: ${Math.sqrt(i).toFixed(2)}");
+//   }
+// }
